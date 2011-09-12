959d34e76448aa589b309e2f6fa9832cce30fc3d
Ryan Williams <ryan.williams@trifectagis.com>
2011-09-13 09:14:47 +1200

    Removed ticket param from redirect URL

--- lib/casclient/frameworks/rails/filter.rb
+++ lib/casclient/frameworks/rails/filter.rb
@@ -358,7 +358,7 @@ module CASClient
               return config[:service_url]
             end
 
-            return controller.env['REQUEST_URI']
+            return controller.env['REQUEST_URI'].gsub(/\?ticket=[^&]*&?/, '?').gsub(/&?ticket=[^&]*/, '')
             
             # FIXME Not sure why this complicated way of generating (an often incorrect) URL is needed. So I just used the above line to replace it all
             # params = controller.params.dup
